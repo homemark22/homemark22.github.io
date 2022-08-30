a11de9135a050ea9ecd7d70d8d390807f339a727
homemark22 <35930501+homemark22@users.noreply.github.com>
2022-08-30 19:07:37 +0800

    Delete antirightclickjs.js

--- antirightclickjs.js
+++
@@ -1,3 +0,0 @@
-<!--Start of anti-right-click Script-->
-<script language="JavaScript"> document.addEventListener("contextmenu", function(e){ e.preventDefault(); }, false); </script>
-<!--End of anti-right-click Script-->
