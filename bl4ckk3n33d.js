d8adc8302882f92cbf51fc0dcae0ca678d325111
homemark22 <35930501+homemark22@users.noreply.github.com>
2024-01-21 01:24:46 +0800

    Update bl4ckk3n33d.js

--- bl4ckk3n33d.js
+++ bl4ckk3n33d.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Check if user has a preference for dark mode
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    // Set initial mode based on user preference
+    if (prefersDarkScheme) {
+        document.body.classList.add('dark-mode');
+    }
+
+    // Toggle dark/light mode on button click
     const toggleButton = document.getElementById('toggle-mode-btn');
     if (toggleButton) {
         toggleButton.addEventListener('click', function () {
             document.body.classList.toggle('dark-mode');
-            toggleButton.classList.toggle('dark-mode');
         });
     }
 });
