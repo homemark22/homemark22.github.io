1d90322016f51248f5d357002e3767fc27682501
homemark22 <35930501+homemark22@users.noreply.github.com>
2024-01-24 21:07:06 +0800

    Update bl4ckk3n33d.js

--- bl4ckk3n33d.js
+++ bl4ckk3n33d.js
@@ -1,4 +1,11 @@
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle("dark-mode");
-}
+const BUTTON = document.querySelector("button");
+const SYNC = document.querySelector("#sync")
+
+const TOGGLE = () => {
+  const IS_PRESSED = BUTTON.matches("[aria-pressed=true]");
+  if (SYNC.checked)
+    document.body.setAttribute("data-dark-mode", IS_PRESSED ? false : true);
+  BUTTON.setAttribute("aria-pressed", IS_PRESSED ? false : true);
+};
+
+BUTTON.addEventListener("click", TOGGLE);
