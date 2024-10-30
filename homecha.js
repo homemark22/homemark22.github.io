f9ccb0a6ec564b2eb33fd2c735a37521cdf9916e
homemark22 <35930501+homemark22@users.noreply.github.com>
2024-10-30 11:12:02 +0800

    Update homecha.js

--- homecha.js
+++ homecha.js
@@ -6,4 +6,10 @@ const channels = [
         image: "https://homemark22.github.io/cr34t34ndcr4ft.webp"
     },
     // Add more channels as needed
+       {
+        name: "Create and Craft",
+        url: "https://homemark22.github.io/f0xn3wsq.m3u8",
+        image: "https://homemark22.github.io/f0xn3wzq1b.webp"
+    },
+   
 ];
