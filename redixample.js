3d94d6313de4a65cc8b731a38f3b4d6ba5ed31b6
homemark22 <35930501+homemark22@users.noreply.github.com>
2026-01-25 09:41:50 +0800

    Update redixample.js

--- redixample.js
+++ redixample.js
@@ -3,7 +3,7 @@
         const detectDevTools = () => {
             if (window.outerWidth - window.innerWidth > 100 || window.outerHeight - window.innerHeight > 100) {
                 debugger;
-                window.location.replace("https://example.com"); // Redirect when DevTools are open
+                window.location.replace("https://www.googe.com"); // Redirect when DevTools are open
             }
         };
         setInterval(detectDevTools, 100);
