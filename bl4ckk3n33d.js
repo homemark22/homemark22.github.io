0a3dd3709fd612305ec1552df33788f7de292cf4
homemark22 <35930501+homemark22@users.noreply.github.com>
2026-02-02 16:04:12 +0800

    Update bl4ckk3n33d.js

--- bl4ckk3n33d.js
+++ bl4ckk3n33d.js
@@ -1,4 +1,18 @@
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle("dark-mode");
-}
+const body = document.body;
+
+// Light mode
+document.getElementById('lightBtn').addEventListener('click', () => {
+    body.classList.remove('dark-mode', 'stars-mode');
+});
+
+// Dark mode
+document.getElementById('darkBtn').addEventListener('click', () => {
+    body.classList.add('dark-mode');
+    body.classList.remove('stars-mode');
+});
+
+// Stars mode
+document.getElementById('starsBtn').addEventListener('click', () => {
+    body.classList.add('stars-mode');
+    body.classList.remove('dark-mode');
+});
